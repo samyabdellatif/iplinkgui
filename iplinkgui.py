27388c980d41431a63ef1519ab22c67680fe500e
Samy Abdellatif <samiahmed086@gmail.com>
2020-12-23 01:06:24 +0300

    extract network interface details

--- iplinkgui.py
+++ iplinkgui.py
@@ -6,6 +6,37 @@ from gi.repository import Gtk
 from subprocess import call,run,STDOUT
 from os import listdir
 
+class interface:
+    def __init__(self,n,t,s):
+        self.iname = n
+        self.itype = t
+        self.istat = s
+    def show(self):
+        print(self.iname,self.itype,self.istat)
+
+#filtering the physical interfaces only using the qdisc (scheduler), for ethernet is fq_codel
+process = subprocess.run("ip link | grep -E 'qdisc fq_codel' | cut -d : -f 2", shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+intFace_ether = list(filter(None,map(str.strip, process.stdout.split("\n"))))
+intF_list = []
+for i in range(len(intFace_ether)):
+    intF = interface(intFace_ether[i],"UP","Ethernet")
+    intF_list.append(intF)
+
+#filtering the physical interfaces only using the qdisc (scheduler), for wifi is mq
+process = subprocess.run("ip link | grep -E 'qdisc mq' | cut -d : -f 2", shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+intFace_wireless = list(filter(None,map(str.strip, process.stdout.split("\n"))))
+for i in range(len(intFace_wireless)):
+    intF = interface(intFace_wireless[i],"UP","WiFi")
+    intF_list.append(intF)
+
+process = subprocess.run("ip link | grep -E 'BROADCAST,MULTICAST,UP,LOWER_UP' | cut -d : -f 2", shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+intFace_up = list(filter(None,map(str.strip, process.stdout.split("\n"))))
+for i in range(len(intFace_up)):
+    intF = interface(intFace_up[i],"CONNECTED","Ethernet")
+    intF_list.append(intF)
+for i in intF_list:
+    i.show()
+
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="network interfaces")
@@ -25,7 +56,6 @@ class MyWindow(Gtk.Window):
             process = subprocess.run("ip link show "+i, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
             output = process.stdout
             print(output)
-
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
