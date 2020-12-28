7c91d07f5ffceb08be82fceca16d1629cef32a57
Samy Abdellatif <samiahmed086@gmail.com>
2020-12-28 03:19:16 +0300

    trying build

---
+++ build/lib/source/__main__.py
@@ -0,0 +1,16 @@
+"""
+Main routine of netui-gtk3.
+:Copyright: © 2020, Samy Abdellatif.
+:License: MIT.
+"""
+from netui import *
+
+
+def main():
+    __name__ = "__main__"
+    win = netUImainWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+if __name__ == '__main__':
+    main()
